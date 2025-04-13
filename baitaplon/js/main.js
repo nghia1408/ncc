e7e9a3e5c5b1d39dd36eb8622b81ee10e9c0c241
Minh Nghiax <96825890+nghia1408@users.noreply.github.com>
2025-04-13 20:38:40 +0700

    Delete baitaplon directory

--- baitaplon/js/main.js
+++
@@ -1,9 +0,0 @@
-// Chờ đến khi trang đã load xong
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    const content = document.getElementById("content");
-  
-    loader.style.display = "none";     // Ẩn loader
-    content.style.display = "block";   // Hiện nội dung
-  });
-  
